5ac2d549cb672f56212646db3386bad05add3e24
Atsushi <atsushi@moongift.jp>
2020-11-05 17:55:50 +0900

    Update script

--- script.rb
+++ script.rb
@@ -3,9 +3,25 @@ require 'json'
 
 def call(env)
   req = Rack::Request.new(env)
-  name = req.params['name'] || 'Default'
+
+  # HTTPメソッドの判別
+  if req.post? || req.put?
+    # ボディデータの取得
+    params = JSON.parse(req.body.read)
+    name = params['name'] || 'POST/PUT'
+  end
+  # GETの場合
+  if req.get?
+    name = req.params['name'] || 'GET'
+  end
+  # 独自のHTTPヘッダーを指定した場合
+  puts req.get_header('HTTP_MYHEADER')
+  # Content-Typeの取得
+  puts req.content_type
+  # HTTPメソッドの取得（GET/POST/PUT/DELETEなど）
+  puts req.request_method
   [200, {"Content-Type" => "application/json"}, [{
     name: name
-  }.to_json]
-  ]
+    }.to_json
+  ]]
 end
